f0c3d5064ae4704aa7812121b952361bf9d7b021
Max Tedford <max.tedford@gmail.com>
2015-07-29 09:55:19 -0600

    add poptart show spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :poptarts, only: [:index]
+  resources :poptarts, only: [:index, :show]
 end
--- spec/controllers/poptarts_controller_spec.rb
+++ spec/controllers/poptarts_controller_spec.rb
@@ -15,4 +15,26 @@ describe PoptartsController do
       expect(poptart['sprinkles']).to eq('red')
     end
   end
+  
+  context '#show' do
+    it 'returns the poptart in question' do
+      strawberry = Poptart.create(flavor: "strawberry", sprinkles: "red")
+      chocolate = Poptart.create(flavor: "chocolate", sprinkles: "brown")
+      
+      get :show, id: strawberry.id, format: :json
+      
+      expect(response).to have_http_status(:ok)
+      
+      poptart_response = JSON.parse(response.body)
+      expect(poptart_response['flavor']).to eq('strawberry')
+      expect(poptart_response['sprinkles']).to eq('red')
+      
+      get :show, id: chocolate.id, format: :json
+
+      expect(response).to have_http_status(:ok)
+      poptart_response = JSON.parse(response.body)
+      expect(poptart_response['flavor']).to eq('chocolate')
+      expect(poptart_response['sprinkles']).to eq('brown')
+    end
+  end
 end
